a13b7c1af03a447709f4fb35d9a0d2529f9c408e
Ahmet Kartal <ahmet38suat38@gmail.com>
2022-11-10 06:02:41 +0300

    Fixed a bug

--- part06-Part06_12.JokeManager/src/main/java/JokeManager.java
+++ part06-Part06_12.JokeManager/src/main/java/JokeManager.java
@@ -2,28 +2,29 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class JokeManager {
+    private ArrayList<String> jokes;
     
-    private ArrayList<String> list;
-
-    public JokeManager() {
-        this.list = new ArrayList<>();
+    public JokeManager(){
+        this.jokes = new ArrayList<>();
     }
     
     public void addJoke(String joke){
-        list.add(joke);
+        this.jokes.add(joke);
     }
-
+    
     public String drawJoke(){
-        if(list.isEmpty())
+        if(this.jokes.isEmpty()){
             return "Jokes are in short supply.";
-
-        Random draw = new Random();
-        int index = draw.nextInt(list.size());
-        return list.get(index);
+        } else {
+            Random draw = new Random();
+            int index = draw.nextInt(jokes.size());
+            return jokes.get(index);
+        }
     }
-
+    
     public void printJokes(){
-        for(String joke: list)
+        for(String joke: this.jokes){
             System.out.println(joke);
+        }
     }
 }
--- part06-Part06_12.JokeManager/src/main/java/UserInterface.java
+++ part06-Part06_12.JokeManager/src/main/java/UserInterface.java
@@ -1,32 +1,43 @@
 import java.util.Scanner;
 
 public class UserInterface {
-    private Scanner scanner;
-    private JokeManager jokes;
 
-    public UserInterface(Scanner scanner, JokeManager jokes) {
-        this.scanner = scanner;
+    private JokeManager jokes;
+    private Scanner scanner;
+
+    public UserInterface(JokeManager jokes, Scanner scanner) {
         this.jokes = jokes;
+        this.scanner = scanner;
     }
 
     public void start() {
-        while(true) {
-            String cmd = scanner.nextLine();
-
-            if(cmd.equals("x")) {
+        System.out.println("What a joke!");
+        
+        while (true) {
+            System.out.println("Commands:");
+            System.out.println(" 1 - add a joke");
+            System.out.println(" 2 - draw a joke");
+            System.out.println(" 3 - list jokes");
+            System.out.println(" X - stop");
+            
+            String command = scanner.nextLine();
+            if(command.equals("X")){
                 break;
-            } else if(cmd.equals("1")) {
+            }
+            
+            if(command.equals("1")){
                 System.out.println("Write the joke to be added:");
                 String joke = scanner.nextLine();
                 jokes.addJoke(joke);
-            } else if(cmd.equals("2")) {
-                System.out.println(jokes.drawJoke());
-            } else if(cmd.equals("3")) {
+            } else if (command.equals("2")){
+                System.out.println("Drawing a joke.");
+                String drawnJoke = jokes.drawJoke();
+                System.out.println(drawnJoke);
+            } else if (command.equals("3")){
                 System.out.println("Printing the jokes.");
                 jokes.printJokes();
-            } else {
-                System.out.println("error");
             }
         }
     }
+
 }
